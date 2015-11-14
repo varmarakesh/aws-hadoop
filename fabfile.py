0b32e0357185c5694af6531c6874a41c079913ac
varmarakesh <rakesh.varma@DALMACC02LW1JVFD58.local>
2015-11-14 05:30:13 -0500

    updated comments for unittest and fabric tasks. commit thru fab task.

--- fabfile.py
+++ fabfile.py
@@ -4,11 +4,11 @@ from fabric.api import *
 from aws_ec2_operations import *
 
 @task
-def local_create_hadoop_cluster():
+def create_hadoop_cluster():
     local('python hadoop_cluster.py')
 
 @task
-def local_test_hadoop_cluster():
+def test_hadoop_cluster():
     local('python -m unittest -v tests.test_aws_ec2_ops')
 
 @task
@@ -19,10 +19,10 @@ def git_commit(msg):
 @task
 def git_commit_push(msg):
     local('git add .')
-    local('git commit -m {0}'.format(msg))
+    local('git commit -m "{0}"'.format(msg))
     local('git config --global user.email "{0}"'.format('varma.rakesh@gmail.com'))
-    local('git config user.name {0}'.format('rakesh varma'))
-    local('git remote add origin https://varmarakesh@github.org/varmarakesh/aws-hadoop.git')
+    local('git config user.name "{0}"'.format('varma.rakesh'))
+    local('git remote set-url origin git@github.com:varmarakesh/aws-hadoop.git')
     local('git push origin master')
     local('git status')
     local('git log --oneline')
--- tests/test_aws_ec2_ops.py
+++ tests/test_aws_ec2_ops.py
@@ -13,11 +13,13 @@ class test_aws_ec2_ops(unittest.TestCase):
         self.ec2 = aws_ec2_operations(config.get('main', 'aws_region'), access_key_id = config.get('main', 'aws_access_key_id'), secret_access_key = config.get('main', 'aws_secret_access_key'))
 
     def test_ec2_instances(self):
+        """Validating ec2 instances by tag names."""
         instances = ['saltmaster', 'hadoopnamenode', 'hadoopsecondarynamenode', 'hadoopslave1', 'hadoopslave2']
         for instance in instances:
             self.assertEqual(instance, self.ec2.getInstance(instance).tags['Name'])
 
     def test_hadoop_hosts_file(self):
+        """Validating aws_hadoop.hosts file ip addresses with actual ec2 instance ip addresses."""
         config = SafeConfigParser()
         config.read('aws_hadoop.hosts')
         instances = ['saltmaster', 'hadoopnamenode', 'hadoopsecondarynamenode', 'hadoopslave1', 'hadoopslave2']
